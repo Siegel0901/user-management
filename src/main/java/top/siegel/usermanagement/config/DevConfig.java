799260725aa9fcf2aee57788cac5bc44bcefebd8
Siegel <2285448337@qq.com>
2024-08-22 20:06:48 +0800

    环境配置

---
+++ src/main/java/top/siegel/usermanagement/config/DevConfig.java
@@ -0,0 +1,19 @@
+package top.siegel.usermanagement.config;
+
+/**
+ * @projectName: user-management
+ * @package: top.siegel.usermanagement.config
+ * @className: DevConfig
+ * @author: Siegel
+ * @createDate: 2024/8/22
+ * @createTime: 19:18
+ * @description:
+ */
+public class DevConfig {
+    // 是否启用gateway，开发环境可设置为false，生产环境设置为true
+    public static final boolean ENABLE_GATEWAY = false;
+
+    // 是否启用security，开发环境可设置为false，生产环境设置为true
+    public static final boolean ENABLE_SECURITY = true;
+
+}
